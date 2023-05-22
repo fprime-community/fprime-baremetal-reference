930a0aa88f2ee3aa3a815f7aa8233d6a0ebab7ef
Ethan Chee <61472206+ethancheez@users.noreply.github.com>
2023-05-22 09:11:24 -0700

    Remove venv

--- venv/lib/python3.10/site-packages/fprime_gds/flask/static/addons/commanding/addon.js
+++
@@ -1,5 +0,0 @@
-// Addon is composed of the following parts
-import "./arguments.js" // Argument and display processing
-import "./command-string.js" // Command string input box
-import "./command-history.js" // Command history list
-import "./command-input.js" // Command input form
